cb46b5db971b7285f730098d56af823669191c5e
David Santiago Tapias <ds.tapias@uniandes.edu.co>
2023-05-12 22:08:26 -0500

    holis

--- widmyauth/widmyauth/autenticacion/logic/autenticacion_logic.py
+++
@@ -1,5 +0,0 @@
-#from ..models import Autenticacion
-
-#def get_adenda(identificador):
- #   adenda = Adenda.objects.get(identificador=identificador)
-  #  return adenda
--- widmyauth/widmyauth/autenticacion/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-       path('', views.autenticacion_buscar, name='autenticacion_buscar'),
-]
